ab8bbe00efd9e7bb050ad5347a820221843c45fa
gojo22k <155639266+gojo22k@users.noreply.github.com>
2024-06-26 11:51:51 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,40 +1,39 @@
-# (c) adarsh-goel
 import os
-from os import getenv, environ
 from dotenv import load_dotenv
-
-
 
 load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '25198711'))
-    API_HASH = str(getenv('API_HASH', '2a99a1375e26295626c04b4606f72752'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'robot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002151954601'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1002151954601").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
+    API_ID = int(os.getenv('API_ID', '25198711'))  # Corrected to use os.getenv instead of getenv
+    API_HASH = os.getenv('API_HASH', '2a99a1375e26295626c04b4606f72752')  # Corrected to use os.getenv
+    BOT_TOKEN = os.getenv('BOT_TOKEN', '')  # Bot token should ideally be securely handled
+    name = os.getenv('name', 'robot')
+    SLEEP_THRESHOLD = int(os.getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(os.getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(os.getenv('BIN_CHANNEL', '-1002151954601'))
+    PORT = int(os.getenv('PORT', '8080'))
+    BIND_ADDRESS = os.getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')  # Corrected BIND_ADDRESS
+    PING_INTERVAL = int(os.getenv("PING_INTERVAL", "1200"))  # Corrected to use os.getenv
+    OWNER_ID = set(int(x) for x in os.getenv("OWNER_ID", "1002151954601").split())  # Corrected to use os.getenv and set conversion
+    NO_PORT = bool(os.getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'ANIFLIX'))
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
+    OWNER_USERNAME = os.getenv('OWNER_USERNAME', 'ANIFLIX')
     
+    if 'DYNO' in os.environ:
+        ON_HEROKU = True
+        APP_NAME = os.getenv('APP_NAME')  # Corrected to use os.getenv
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    
+    FQDN = os.getenv('FQDN', BIND_ADDRESS) if not ON_HEROKU or os.getenv('FQDN') else APP_NAME + '.herokuapp.com'
+    HAS_SSL = bool(os.getenv('HAS_SSL', False))
+    
     if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
+        URL = f"https://{FQDN}/"  # Using f-string for URL formatting
     else:
-        URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Aniflix_Official'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+        URL = f"http://{FQDN}/"  # Using f-string for URL formatting
+    
+    DATABASE_URL = os.getenv('DATABASE_URL', 'mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix')
+    UPDATES_CHANNEL = os.getenv('UPDATES_CHANNEL', 'Aniflix_Official')
+    BANNED_CHANNELS = list(set(int(x) for x in os.getenv("BANNED_CHANNELS", "-1001362659779").split()))  # Corrected to use os.getenv and set conversion
